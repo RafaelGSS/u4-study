31357686fe2562e2444cd9d0832fede7942a8275
RafaelGSS <rafael.nunu@hotmail.com>
2020-04-26 16:10:47 -0300

    nitial commit

---
+++ Source/Udemy2/Udemy2.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Udemy2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Udemy2, "Udemy2" );
